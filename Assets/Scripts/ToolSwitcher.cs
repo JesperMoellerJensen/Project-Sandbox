01a722cbee1a32ce6bb43a1ac31ff7eac160327f
Jesper <jesp626a@edu.eal.dk>
2018-12-21 11:59:29 +0100

    Added tool switcher started on gravity gun

---
+++ Assets/Scripts/ToolSwitcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Tools;
+using UnityEngine;
+
+public class ToolSwitcher : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            GravityGun();
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            WeldTool();
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            Thruster();
+    }
+
+    void GravityGun()
+    {
+        GetComponent<Tool_GravityGun>().enabled = true;
+        GetComponent<Tool_Thruster>().enabled = false;
+        GetComponent<Tool_Weld>().enabled = false;
+    }
+
+    void WeldTool()
+    {
+        GetComponent<Tool_Weld>().enabled = true;
+        GetComponent<Tool_Thruster>().enabled = false;
+        GetComponent<Tool_GravityGun>().enabled = false;
+    }
+
+    void Thruster()
+    {
+        GetComponent<Tool_Thruster>().enabled = true;
+        GetComponent<Tool_Weld>().enabled = false;
+        GetComponent<Tool_GravityGun>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/Tools/Tool_GravityGun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool_GravityGun : Tool {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
